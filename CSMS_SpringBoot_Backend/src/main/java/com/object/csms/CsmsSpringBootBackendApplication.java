cd88ff832da002bb8d64bb5eac67d4eb04e9841a
mayurpawar0305 <mayurpawar0305@gmail.com>
2022-09-07 11:21:38 +0530

    all basic structure of spring boot and reactjs is done

---
+++ CSMS_SpringBoot_Backend/src/main/java/com/object/csms/CsmsSpringBootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.object.csms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsmsSpringBootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsmsSpringBootBackendApplication.class, args);
+	}
+
+}
