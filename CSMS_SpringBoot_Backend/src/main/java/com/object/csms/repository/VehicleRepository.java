b7ad756a64107636df42fef3d650747e4ea909ec
mayurpawar0305 <mayurpawar0305@gmail.com>
2022-09-12 11:44:56 +0530

    vehivle class

--- CSMS_SpringBoot_Backend/src/main/java/com/object/csms/repository/VehicleRepository.java
+++ CSMS_SpringBoot_Backend/src/main/java/com/object/csms/repository/VehicleRepository.java
@@ -3,7 +3,6 @@ package com.object.csms.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.object.csms.entity.Company;
 import com.object.csms.entity.Vehicles_Details;
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicles_Details, Integer> {
