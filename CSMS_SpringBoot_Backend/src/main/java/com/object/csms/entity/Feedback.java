717d063451081843760323cd41db6bdb3864d508
shri <kekapures@gmail.com>
2022-09-17 00:47:35 +0530

    ch1

--- CSMS_SpringBoot_Backend/src/main/java/com/object/csms/entity/Feedback.java
+++ CSMS_SpringBoot_Backend/src/main/java/com/object/csms/entity/Feedback.java
@@ -1,11 +1,16 @@
 package com.object.csms.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name="feedback")
@@ -14,8 +19,46 @@ public class Feedback {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int feedbackId;
 	
-	@Column(nullable = false, length = 100,name="")
-    private String userPassword;
+	@Column(name="feedback_description",nullable=false)
+	private String feedbackDiscription;
+	
+	@Column(name="customer_id",nullable=false)
+	private int customerId;
+
+	public Feedback(int feedbackId, String feedbackDiscription, int customerId) {
+		super();
+		this.feedbackId = feedbackId;
+		this.feedbackDiscription = feedbackDiscription;
+		this.customerId = customerId;
+	}
+
+	public int getFeedbackId() {
+		return feedbackId;
+	}
+
+	public void setFeedbackId(int feedbackId) {
+		this.feedbackId = feedbackId;
+	}
+
+	public String getFeedbackDiscription() {
+		return feedbackDiscription;
+	}
+
+	public void setFeedbackDiscription(String feedbackDiscription) {
+		this.feedbackDiscription = feedbackDiscription;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomer(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public Feedback() {
+		super();
+	}
      
 	
 }
